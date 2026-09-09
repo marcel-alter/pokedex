5660dadedc1de76002d145b356b848876caefba5
marcel-alter <h2-o@hotmail.de>
2026-09-09 23:37:47 +0200

    switched to solution code, cache not implemented yet

--- main.go
+++ main.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"time"
+
+	"github.com/marcel-alter/pokedexcli/internal/pokeapi"
+	//"github.com/marcel-alter/pokedexcli/internal/pokecache"
 )
 
-func cleanInput(text string) []string {
-	textSlice := strings.Fields(strings.ToLower((text)))
-	return textSlice
-}
-
 func main() {
-	/*inputString := strings.Join(os.Args[1:], " ")
-	fmt.Println("os.Args input was: ", inputString)
-	fmt.Println("cleanInput: ", cleanInput(inputString))*/
-	initCommands()
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		cleanToken := cleanInput(scanner.Text())
-		if len(cleanToken) == 0 {
-			continue
-		}
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("Invalid input: %s", err)
-		}
-		if com, ok := configCom.commands[cleanToken[0]]; ok {
-			err := com.callback(&configCom)
-			if err != nil {
-				fmt.Println("Error: %w", err)
-			}
-		} else {
-			fmt.Print("Unknown command\n")
-		}
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		commands:      getCommands(),
+		pokeapiClient: pokeClient,
 	}
+
+	startRepl(cfg)
 }
